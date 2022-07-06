75984fe354ab539355aa99d244bde107d313a2d0
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-06 16:00:45 +0530

    headder in progress

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import logo from '../images/insta_logo.png';
+import logo2 from '../images/insta_logo2.jpeg';
 import profilePic from '../images/profile_pic.jpg';
 
 import {ReactComponent as Home} from '../svgs/home.svg';
@@ -10,14 +11,16 @@ import {ReactComponent as Activity} from '../svgs/activity.svg';
 
 export default function Header(){
     return(
-        <section className="header">
-            <div className="bg-white flex flex-row items-center">
+        <section id="header" className='w-full border-2 border-header_border'>
+            <div className="bg-white h-14 w-3/4 flex flex-row items-center justify-evenly mx-auto">
                 {/**Logo image */}
-                <img className="w-20 h-20" src={logo} />
+                <img className="w-28 h-12" src={logo} />
                 {/**Header search */}
-                    <input className='p-2 border-2 rounded-sm h-8' placeholder='Search'/>
+                <div className='w-1/3 justify-items-center'>
+                    <input className=' p-2 border-2 rounded-lg h-8 hidden md:block' placeholder='Search'/>
+                    </div>
                 {/**Header Links */}
-                <div className="flex flex-row justify-center">
+                <div className="flex flex-row w-1/3 justify-evenly">
                 <Home className='icon'/>                    
                 <Inbox className='icon'/>                    
                 <Add className='icon'/>                    
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        header_border:"#dbdbdb"
+      }
+    },
   },
   plugins: [],
 }
