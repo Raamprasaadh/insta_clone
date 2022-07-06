376331f0d1022393cdab9ead5666a3dc774f85dc
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-07 00:24:11 +0530

    adds feed skeleton

--- src/Components/Body-Left.js
+++ src/Components/Body-Left.js
@@ -5,6 +5,10 @@ import status3 from "../images/suggest_suraj.jpg";
 import status4 from "../images/suggest_flash.jpg";
 import status5 from "../images/suggest_anthro.jpg";
 
+import pp1 from "../images/pp_1.jpg";
+import pp2 from "../images/pp_2.jpg";
+import post1 from "../images/post1.jpeg";
+import post2 from "../images/post2.jpg";
 
 function BodyLeft() {
 
@@ -15,12 +19,16 @@ function BodyLeft() {
     { img: status4 },
     { img: status5 }
   ];
-
+  const feedData = [
+    { img: pp1,name:"webdesignuniversity", postImg:post1, postBody:"Generate QR Code with simple JavaScript 🔥" }, 
+    { img: pp2,name:"*moviequotesworld*", postImg:post2, postBody:"Everyone's talking about the #StrangerThings kids grew up so much and nobody's talking about how amazing #Hopper had... more" }, 
+    
+  ];
   return (
     <section id="bodyMain">
-      <div className="flex flex-col space-y-4 grow-1 min-h-full max-w-1/3 border-4 border-black">
+      <div className="flex flex-col space-y-4 grow-1 h-full w-2/3 max-w-2/3">
         {/**statusbar */}
-        <div className="flex flex-row mx-10 bg-white border-2 border-header_border rounded-lg shadow">
+        <div className="flex flex-row bg-white border-2 border-header_border rounded-lg shadow">
           {
             statusData.map((datum, index)=>{
               return<img key={index}className="rounded-full w-14 h-14" src={datum.img} alt="status1"/>
@@ -28,7 +36,22 @@ function BodyLeft() {
           }
         </div>
         {/**feed */}
-        Main Body Main Body Main Body Main Body Main Body Main BodyMain BodyMain BodyMain Body Main BodyMain BodyMain Body
+        <div className="flex flex-col">
+          {/**feed card */
+          feedData.map((data,index)=>{
+            return <div className="rounded-lg border-2 border-header_border" key={index}>
+              {/**feed card head */}
+              <div className="flex space-x-5 flex-row w-2/3 h-10 items-center justify evenly">
+                <img className="w-1/10 h-10 rounded-full justify-self-center" src={data.img} alt="post_profile"/>
+                <div className="w-7/10 justify-self-center">{data.name}</div>
+                <div className="w-1/10 icon justify-self-center">...</div>
+              </div>
+              {/**feed card img */}
+              <img className="w-2/3" src={data.postImg} alt="post_img"/>
+            </div>
+          })
+          }
+        </div>
       </div>
     </section>
   );
--- src/Components/Body-Right.js
+++ src/Components/Body-Right.js
@@ -14,33 +14,33 @@ function BodyRight() {
   ];
   return (
     <section id="bodyFooter">
-      <div className="flex-col space-y-4 md:flex max-w-1/3 max-h-2/3">
+      <div className="flex-col space-y-6 md:flex max-w-1/2 max-h-2/3">
         {/**profile */}
-        <div className="flex flex-row space-x-4 items-center w-full">
+        <div className="flex flex-row space-x-2 items-center w-full">
           <div className="w-1/8">
             <img
-              className="w-1/8 h-auto rounded-full"
+              className="w-14 h-auto rounded-full"
               src={profilePic}
               alt="profile_pic"
             />
           </div>
-          <div className="w-4/8 shrink-0">
+          <div className="w-2/3 space-y-2 shrink-0">
             <div className="w-full h-4 text-suggest_pf_uname font-medium text-sm">
               badlytimedjokes
             </div>
             <div className="w-full h-4 text-header_text text-sm">
-              Ram Prasadh Natarajan
+              RamPrasadh Natarajan
             </div>
           </div>
-          <div className="w-2/8 text-suggest_link text-sm">Switch</div>
+          <div className="w-1/12 text-suggest_link text-xs">Switch</div>
         </div>
         {/**Suggestion */}
-        <div className="flex flex-col space-y-2 w-full">
-          <div className="flex flex-row items-center w-full">
-            <div className="w-7/8 font-base text-header_text justify-left">
+        <div className="flex flex-col space-y-1 w-full">
+          <div className="flex flex-row space-x-4 items-center">
+            <div className="w-8/12 font-base text-header_text text-left">
               Suggestions for you
             </div>
-            <div className="w-1/8 text-xs font-medium justify-right">
+            <div className="w-3/12 text-xs font-medium text-right">
               See all
             </div>
           </div>
@@ -67,14 +67,14 @@ function BodyRight() {
                     </div>
                   </div>
 
-                  <div className="w-1/4 text-suggest_link text-sm text-right">Follow</div>
+                  <div className=" text -right w-1/4 text-suggest_link text-sm text-right">Follow</div>
                 </div>
               );
             })}
           </div>
         </div>
         {/**Footer */}
-        <div className="h-2/6 w-full text-xs text-footer_text font-bold">
+        <div className="h-2/6 w-full text-xs text-footer_text font-medium space-y-4 mb-5">
           <ul className="space-x-1 divide-x-2 divide-solid   ">
             <li className="inline">About</li>
             <li className="inline">Help</li>
