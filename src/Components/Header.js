e1379c71f524183809842ac1a5f4667a3b9b67a1
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-06 15:23:03 +0530

    Header basic structure

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,31 @@
+import logo from '../images/insta_logo.png';
+import profilePic from '../images/profile_pic.jpg';
+
+import {ReactComponent as Home} from '../svgs/home.svg';
+import {ReactComponent as Inbox} from '../svgs/inbox.svg';
+import {ReactComponent as Add} from '../svgs/addPost.svg';
+import {ReactComponent as Find} from '../svgs/findPeople.svg';
+import {ReactComponent as Activity} from '../svgs/activity.svg';
+
+
 export default function Header(){
     return(
         <section className="header">
-            <div className="bg-blue-600">Header</div>
+            <div className="bg-white flex flex-row items-center">
+                {/**Logo image */}
+                <img className="w-20 h-20" src={logo} />
+                {/**Header search */}
+                    <input className='p-2 border-2 rounded-sm h-8' placeholder='Search'/>
+                {/**Header Links */}
+                <div className="flex flex-row justify-center">
+                <Home className='icon'/>                    
+                <Inbox className='icon'/>                    
+                <Add className='icon'/>                    
+                <Find className='icon'/>                    
+                <Activity className='icon'/>                    
+                <img className='w-6 h-6 rounded-full' src={profilePic} />
+                </div>
+            </div>
         </section>
     )
 }
