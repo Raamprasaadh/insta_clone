ab27ad41ceb83f26535aff1b77b4c377a48e5b51
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-06 14:30:53 +0530

    adds readme & todos in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/**header */}
+      {/** 
+       * body
+       * left component
+       * **status bar - slider
+       * **posts infinite scrolling
+       * right component
+       * **userprofile
+       * **suggestions for you bar
+       * **footer
+      */}
+      {/** */}
     </div>
   );
 }
